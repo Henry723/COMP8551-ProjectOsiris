c7f7aa47e567c024250dc23c52cb93b304e09f3e
Kurtis Lawson <kurtlawson17@gmail.com>
2021-10-05 10:15:20 -0700

    feat(RenderSystem): Initial port of 3D Renderer code from working C++ example. Currently compiles, but file IO for loading shaders, textures, and models is being debugged.

--- src/GameControl.cpp
+++ src/GameControl.cpp
@@ -1,5 +1,11 @@
 #include "GameControl.h"
 
+// TEMP - Handled by Asset System?
+const char* modelSource = "./Models/Wayfarer.obj";
+const char* vertSource = "./Shaders/Default.vert";
+const char* fragSource = "./Shaders/Default.frag";
+const char* texSource = "./Textures/container.jpg";
+
 GameControl::GameControl(GLFWwindow* window, string filename)
 {
   // Create systems
@@ -12,6 +18,10 @@ GameControl::GameControl(GLFWwindow* window, string filename)
   entityx::Entity entity = entities.create();
   entity.assign<Window>(window);
   entity.assign<Color>(0.2f, 0.3f, 0.3f, 1.0f);
+
+  // TEST - Create a 3D model entity for the RenderSystem to use
+  entityx::Entity playerEntity = entities.create();
+  playerEntity.assign<Model3D>(modelSource, vertSource, fragSource, texSource);
 }
 
 void GameControl::Update(TimeDelta dt)
--- src/Renderer3D/RenderSystem.cpp
+++ src/Renderer3D/RenderSystem.cpp
@@ -2,21 +2,31 @@
 
 void RenderSystem::update(EntityManager& es, EventManager& ev, TimeDelta dt)
 {
-  // Create component handles to filter components
-  ComponentHandle<Color> hcolor;
-  ComponentHandle<Window> hwindow;
+	// Create component handles to filter components
+	ComponentHandle<Color> hcolor;
+	ComponentHandle<Window> hwindow;
+	ComponentHandle<Model3D> hmodel;
 
-  // Loop through components
-  auto en = es.entities_with_components(hwindow, hcolor);
-  for (auto entity = en.begin(); entity != en.end(); ++entity)
-  {
-    Color *col = (*entity).component<Color>().get();
-    // rendering commands
-    // ...
-    glClearColor(col->red, col->green, col->blue, col->alpha); // state-setting function of OpenGL
-    glClear(GL_COLOR_BUFFER_BIT); // state-using function. Uses the current state defined to retrieve the clearing color.
+	// Loop through window components
+	auto en = es.entities_with_components(hwindow, hcolor);
+	for (auto entity = en.begin(); entity != en.end(); ++entity)
+	{
+		Color *col = (*entity).component<Color>().get();
+		// rendering commands
+		// ...
+		glClearColor(col->red, col->green, col->blue, col->alpha); // state-setting function of OpenGL
+		glClear(GL_COLOR_BUFFER_BIT); // state-using function. Uses the current state defined to retrieve the clearing color.
 
-    //update color buffer (a 2D buffer that contains color values for each pixel) to render during this iteration and show it as output to the screen.
-    glfwSwapBuffers((*entity).component<Window>().get()->window);
-  }
+		//update color buffer (a 2D buffer that contains color values for each pixel) to render during this iteration and show it as output to the screen.
+		glfwSwapBuffers((*entity).component<Window>().get()->window);
+	}
+
+	// Loop through Model3D components
+	auto modelEntities = es.entities_with_components(hmodel);
+	for (auto entity = modelEntities.begin(); entity != modelEntities.end(); ++entity) {
+		Model3D* model = (*entity).component<Model3D>().get();
+
+		//cout << "Drawing model " << model.name << endl;
+		model->Draw();
+	}
 }
