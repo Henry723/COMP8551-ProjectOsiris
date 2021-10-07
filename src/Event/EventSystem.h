97cf925d04b57d30fdb975301e059529df48db8d
Henry Zhang <henryzhang1020@gmail.com>
2021-10-07 16:20:35 -0700

    Example EventSystem implemented

--- src/Event/EventSystem.h
+++ src/Event/EventSystem.h
@@ -1,6 +1,35 @@
 #pragma once
 #include "entityx/Event.h"
+#include <iostream>
 
+using namespace std;
 using entityx::EventManager;
 using entityx::Event;
 using entityx::Receiver;
+
+struct ExampleEvent {
+	explicit ExampleEvent(int a, int b) :a(a) , b(b) {}
+	int a, b;
+};
+
+class ExampleEmitterSystem {
+	EventManager em;
+public:
+	void emitSample() {
+		em.emit<ExampleEvent>(1, 2);
+	}
+};
+
+class ExampleListenerSystem : public Receiver<ExampleListenerSystem> {
+	EventManager em;
+	int total;
+
+	ExampleListenerSystem() {
+		em.subscribe<ExampleEvent>(*this);
+	}
+
+	void recieve(const ExampleEvent &ee) {
+		total += ee.a * ee.b;
+		cout << total << endl;
+	}
+};
