fd4d4ed377ef9b16011810165d55c1007da831f8
kolsson3 <55680535+kolsson3@users.noreply.github.com>
2021-10-09 10:44:41 -0700

    adding some method stubs, commented out for now until PhysicsBody and Vector are ready

--- src/Physics/PhysicsEngine.h
+++ src/Physics/PhysicsEngine.h
@@ -6,9 +6,19 @@ class PhysicsEngine : public System<PhysicsEngine>
 {
 public:
 	PhysicsEngine();
+	~ PhysicsEngine();
 	void test();
 	void update(EntityManager&, EventManager&, TimeDelta) override;
-	b2World* GetWorld() { return world; };
+	//PhysicsBody addBody(float size);
+	//bool removeBody(PhysicsBody body);
+	//void setBodyPosition(PhysicsBody body, Vector Position);
+	//void setBodyVelocity(PhysicsBody body, Vector velocity);
+	//void setBodyImpulse(PhysicsBody body, Vector force);
+	//void setBodyTransform(PhysicsBody body, Vector transform);
+	//Vector getBodyPosition(PhysicsBody body);
+	//Vector getBodyVelocity(PhysicsBody body);
+	//Vector getBodyImpulse(PhysicsBody body);
+	//Vector getBodyTransform(PhysicsBody body);
 private:
 	b2World* world;
 };
