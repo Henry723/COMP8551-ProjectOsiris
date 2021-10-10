0c87ba08b7a5a5d6e4ffa7f9b887a6c42b9323c7
AugeyT <71095950+AugeyT@users.noreply.github.com>
2021-10-10 03:37:01 -0700

    Memory manager fully implemented and tested
    - Memory Manager Pool is full implemented
    - Tester complete and passed testing all functional tests.
    - Waiting for code review before submitting to main

--- src/GameControl.cpp
+++ src/GameControl.cpp
@@ -1,5 +1,9 @@
 #include "GameControl.h"
 
+// TESTING -- Start --
+#include "MemoryManager/CMMPoolTester.h"
+// TESTING -- End --
+
 GameControl::GameControl(GLFWwindow* window, string filename)
 {
   // Create systems
@@ -10,6 +14,16 @@ GameControl::GameControl(GLFWwindow* window, string filename)
   //Testing Scene Manager
   glfwSetWindowTitle(window, filename.c_str());
 
+  // TESTING -- Start --
+  {
+      CMMPoolTester class_test;
+      class_test.run_test();
+      int fail_count = class_test.failed();
+      int pass_count = class_test.passed();
+      bool CMMPoolTest_success = class_test.tests_all_passed();
+  }
+  // TESTING -- End --
+ 
   // TODO make it easier to create entities
   // Create entities
   entityx::Entity entity = entities.create();
