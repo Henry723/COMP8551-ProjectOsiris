7bb67a085f569eb264efcdebd1fd60e82a973300
Kurtis Lawson <kurtlawson17@gmail.com>
2021-10-10 11:17:54 -0700

    feat(RenderSystem): Moved draw function into the system level to abstract behavior way from Model3D component

--- src/Renderer3D/RenderSystem.h
+++ src/Renderer3D/RenderSystem.h
@@ -5,5 +5,8 @@ class RenderSystem : public System<RenderSystem>
 {
 public:
   void update(EntityManager&, EventManager&, TimeDelta) override;
+
+private:
+  void draw(Model3D* modelComponent);
 };
 
