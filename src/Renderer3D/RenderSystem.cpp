ac13eafb4168d49e5a449c4ac3457b1687afe354
Henry Zhang <henryzhang1020@gmail.com>
2021-10-21 14:49:09 -0700

    Specular light added

--- src/Renderer3D/RenderSystem.cpp
+++ src/Renderer3D/RenderSystem.cpp
@@ -1,5 +1,5 @@
 #include "RenderSystem.h"
-// lighting
+
 void RenderSystem::update(EntityManager& es, EventManager& ev, TimeDelta dt)
 {
 	// Create component handles to filter components
@@ -71,16 +71,28 @@ void RenderSystem::draw(Model3D* modelComponent, Camera* cameraComponent)
 	//		This is called FOR EACH object we want to draw this frame.
 	// 1. Choose the shader to use
 	modelComponent->shader_program.use();
-	
-	//Ambient lights
-	//sets ambient light value in shader (r, g, b)
+
+	//Camera
+	//sets the camera location which calculates the camera position relative to the models
+	modelComponent->shader_program.setVec3("positionOffset", cameraComponent->Position.x, cameraComponent->Position.y, cameraComponent->Position.z);
+
+	//Default ambient lights
+	//sets the ambient light value in shader (r, g, b)
 	modelComponent->shader_program.setVec3("light.ambient", 0.3f, 0.2f, 0.2f);
 
-	//Diffuse lights
-	//sets position of the diffuse lighting
+	// Default diffuse lights
+	//sets the position of the diffuse lighting
 	modelComponent->shader_program.setVec3("light.position", 0.0f, 0.0f, 1.0f);
-	//sets diffuse light value in shader (r, g, b)
+	//sets the diffuse light value in shader (r, g, b)
 	modelComponent->shader_program.setVec3("light.diffuse", 0.5f, 0.5f, 0.5f);
+	//sets the specular light value of light in shader (r, g, b)
+	modelComponent->shader_program.setVec3("light.specular", 1.0f, 1.0f, 1.0f);
+
+	// Default material properties
+	//sets the specular light value of the material in shader (r, g, b)
+	modelComponent->shader_program.setVec3("material.specular", 1.0f, 1.0f, 1.0f);
+	//sets the material shininess value in shader, the higher the value the less "shiny" it is.
+	modelComponent->shader_program.setFloat("material.shininess", 10.0f);
 
 	//create perspective matrix and set it in shader
 	glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)800 / (float)600, 0.1f, 100.0f);
--- src/Renderer3D/Shaders/Default.frag
+++ src/Renderer3D/Shaders/Default.frag
@@ -7,6 +7,11 @@ struct Light {
     vec3 specular;
 };
 
+struct Material {
+    vec3 specular;    
+    float shininess;
+}; 
+
 out vec4 FragColor;
 
 in vec3 vertexColor; // the input variable from the vertex shader (same name and type as in vert)
@@ -15,7 +20,8 @@ in vec3 FragPos; // the input variable from the vertex shader (same name and typ
 
 uniform vec3 positionOffset; // The global "uniform" position variable, possibly updated per frame through an external loop?
 uniform sampler2D texture1; // This uniform allows us to assign our texture as output for our Fragment shader
-uniform Light light; // This uniform creates our light object for the ambient light for now
+uniform Light light; // This uniform creates our light object for ambient, diffuse, and specular lights.
+uniform Material material; // This uniform creates our material object for the models we imported
 
 void main()
 {
@@ -28,8 +34,14 @@ void main()
     float diff = max(dot(normal, lightDirection), 0.0);
     vec3 diffuse = vec3(light.diffuse) * diff * texture(texture1, TexCoord).rgb;
 
+    // Specular lighting to show how light reflects when it comes to each faces
+    vec3 viewDir = normalize(positionOffset - FragPos);
+    vec3 reflectDir = reflect(-lightDirection, normal);  
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    vec3 specular = light.specular * (spec * material.specular);  
+
     //the final results of the color 
-    vec3 result = ambient + diffuse;
+    vec3 result = ambient + diffuse + specular;
 
     FragColor = vec4(result, 1.0);
 }
