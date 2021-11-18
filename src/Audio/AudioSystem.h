3fcf039c4050f830f7c2f95c5fb387a50ce45dc0
Houman Gholami <hgholami1@my.bcit.ca>
2021-11-17 17:01:08 -0800

    Began Implementation of FMOD

--- src/Audio/AudioSystem.h
+++ src/Audio/AudioSystem.h
@@ -1,6 +1,42 @@
 #pragma once
 #include "../components.hpp"
 #include <FMOD/fmod_studio_common.h>
+#include <FMOD/fmod_studio.hpp>
+#include <string>
+#include <map>
+#include <vector>
+#include <math.h>
+#include <iostream>
+
+using namespace std;
+
+struct Vector3 {
+  float x;
+  float y;
+  float z;
+};
+
+struct Implementation {
+  Implementation();
+  ~Implementation();
+
+  void Update();
+
+  FMOD::Studio::System* mpStudioSystem;
+  FMOD::System* mpSystem;
+
+  int mnNextChannelId;
+
+  typedef map<string, FMOD::Sound*> SoundMap;
+  typedef map<int, FMOD::Channel*> ChannelMap;
+  typedef map<string, FMOD::Studio::EventInstance*> EventMap;
+  typedef map<string, FMOD::Studio::Bank*> BankMap;
+
+  BankMap mBanks;
+  EventMap mEvents;
+  SoundMap mSounds;
+  ChannelMap mChannels;
+};
 
 class AudioSystem : public System<AudioSystem> {
 public:
