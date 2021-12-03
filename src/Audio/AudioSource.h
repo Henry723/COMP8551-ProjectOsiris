1d635b9ebaa02b23d1953a9353f91226f35abc01
Houman Gholami <35938542+hgholami@users.noreply.github.com>
2021-12-03 14:26:53 -0800

    Audio played at various triggers

--- src/Audio/AudioSource.h
+++ src/Audio/AudioSource.h
@@ -4,10 +4,11 @@ static const char* src_aud_bgMusic = "./src/Audio/audio_files/bg_music.wav"; // 
 static const char* src_aud_lose = "./src/Audio/audio_files/lose.wav"; // Losing Audio
 static const char* src_aud_win = "./src/Audio/audio_files/windoot.wav"; // Winning Audio
 
-static const char* src_aud_malePain = "./src/Audio/audio_files/malepain.wav"; // Player Hit
-static const char* src_aud_whoosh = "./src/Audio/audio_files/whoosh.wav"; // Sword Whoosh
+static const char* src_aud_playerHit = "./src/Audio/audio_files/malepain.wav"; // Player Hit
+static const char* src_aud_playerAttack = "./src/Audio/audio_files/whoosh.wav"; // Sword Whoosh
 
-static const char* src_aud_buzz = "./src/Audio/audio_files/buzz.wav"; // Bug Buzzing
-static const char* src_aud_splat = "./src/Audio/audio_files/splat.wav"; // Bug Splat
+static const char* src_aud_enemyAttack = "./src/Audio/audio_files/buzz.wav"; // Bug Buzzing
+static const char* src_aud_enemyHit = "./src/Audio/audio_files/splat.wav"; // Bug Splat
+static const char* src_aud_enemyDeath = "./src/Audio/audio_files/splat.wav"; // Bug Splat
 
-static const char* src_aud_coin = "./src/Audio/audio_files/retro_coin.wav"; // Coin Ding
+static const char* src_aud_treasurePickup = "./src/Audio/audio_files/retro_coin.wav"; // Coin Ding
